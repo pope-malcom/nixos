13ea8b0db212d7440d97fa8d02d2031e272a39b7
nixos-flake-update <pope.malcom@proton.me>
2026-01-05 20:29:37 +0000

    Added new search engines for NixOS Options and NixOS Wiki

--- home/browsers/librewolf.nix
+++ home/browsers/librewolf.nix
@@ -12,7 +12,7 @@
         search.force = true; # Replace config with one defined here
         search.engines = {
           nix-packages = {
-            name = "Nix Packages";
+            name = "NixOS Packages";
             urls = [{
               template = "https://search.nixos.org/packages";
               params = [
@@ -23,6 +23,28 @@
 
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@np" ];
+          };
+
+          nix-options = {
+            name = "NixOS Options";
+            urls = [{
+              template = "https://search.nixos.org/options";
+              params = [
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@no" ];
+          };
+
+          nixos-wiki = {
+            name = "NixOS Wiki";
+            urls = [{ 
+              template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; 
+            }];
+            iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+            definedAliases = [ "@nw" ];
           };
         };
       };
