e5b62dfaca6f02f200e6c3319de2b0ec77f54613
nixos-flake-update <pope.malcom@proton.me>
2026-01-05 19:29:50 +0000

    Changed cursor

--- system/modules/stylix.nix
+++ system/modules/stylix.nix
@@ -11,5 +11,12 @@
     
     image = ../../wallpaper.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/penumbra-dark-contrast-plus-plus.yaml";
+
+    # https://github.com/simtrami/posy-improved-cursor-linux
+    cursor = {
+      name = "Posy_Cursor_Mono_Black";
+      package = pkgs.posy-cursors;
+      size = 22;
+    };
   };
 }
