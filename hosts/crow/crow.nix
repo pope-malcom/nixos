3af587f12788716ca2f587b9a7d9b92bbd138c4b
nixos-flake-update <pope.malcom@proton.me>
2026-01-08 12:41:51 +0100

    Added plymouth to LUKS password screen

--- hosts/crow/crow.nix
+++ hosts/crow/crow.nix
@@ -24,11 +24,13 @@
     plymouth.enable = true;
 
     # Silent boot
-    consoleLogLevel = 0;
+    consoleLogLevel = 3;
     initrd.verbose = false;
+    initrd.systemd.enable = true;
     kernelParams = [
       "quiet"
       "splash"
+      "intremap=on"
       "boot.shell_on_fail"
       "udev.log_priority=3"
       "rd.systemd.show_status=auto"
