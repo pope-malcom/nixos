d2780bfb58d093737602c7c6b6c4e3bac8eee3f7
pope-malcom <pope.malcom@proton.me>
2026-03-05 08:34:14 +0100

    Removed oscilloscope rule

--- system/hardware/udev.nix
+++ system/hardware/udev.nix
@@ -4,9 +4,6 @@
 
 {
   services.udev.extraRules = ''
-    # Siglent 1204X-E oscilloscope
-    SUBSYSTEM=="usb", ATTR{idVendor}=="f4ec", ATTR{idProduct}=="ee38", MODE="0666"
-
     # Exibel USB Ethernet adapter
     SUBSYSTEM=="usb", ATTR{idVendor}="0b95", ATTR{idProduct}=="1790", MODE="0666"
   '';
