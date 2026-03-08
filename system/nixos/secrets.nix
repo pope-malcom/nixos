2b5a8fa665d265b681e9148932a39804d8d89dfd
pope-malcom <pope.malcom@proton.me>
2026-03-08 11:53:18 +0100

    Enabled auto-unlocking keyring by modifying greetd pam module

--- system/nixos/secrets.nix
+++ system/nixos/secrets.nix
@@ -1,5 +1,5 @@
 # Setup keyring and secret management
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   services.gnome = {
@@ -13,7 +13,28 @@
   # Enable seahorse for GUI keyring management 
   programs.seahorse.enable = true;
 
+  # Unlock keyring on login
+  security.pam.services = {
+    login.enableGnomeKeyring = true;
+    # All of this is needed because greetd does not work well with unlocking keyrings automatically
+    # TODO: replace greetd with a more compliant greeter
+    greetd =
+      let
+        pam_fde_boot_pw = pkgs.callPackage ./pam_fde_boot_pw.nix {};
+      in
+      lib.mkIf config.services.greetd.enable {
+        text = ''
+          auth          substack        login
+          account       include         login
+          password      substack        login
+          session       optional        ${pam_fde_boot_pw}/lib/security/pam_fde_boot_pw.so inject_for=gkr
+          session       include         login
+        '';
+      };
+  };
+
   environment.systemPackages = [
+    # Secret-tool
     pkgs.libsecret
   ];
 }
