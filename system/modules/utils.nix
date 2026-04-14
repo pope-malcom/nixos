69ea5e7fc50689bd4493b775f91067dfd060d405
pope-malcom <pope.malcom@proton.me>
2026-04-14 07:54:56 +0200

    Removed JLINK tools, they can be added on a per-project basis

--- system/modules/utils.nix
+++ system/modules/utils.nix
@@ -24,5 +24,13 @@
     # System packages to install
     pavucontrol
     teams-for-linux
+
+    segger-jlink
+    gcc-arm-embedded
   ];
+  
+  # TODO Look at moving this to home-manager
+  nixpkgs.config.segger-jlink.acceptLicense = true;
+  # TODO check for progress on https://github.com/NixOS/nixpkgs/pull/447868
+  nixpkgs.config.permittedInsecurePackages = [ "segger-jlink-qt4-874" ];
 }
