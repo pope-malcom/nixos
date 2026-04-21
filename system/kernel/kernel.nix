7e85e4f5cb4daa9f5db9d27e49f7bdf89e0a6781
pope-malcom <pope.malcom@proton.me>
2026-04-21 20:45:14 +0200

    Reverted to default kernel version, latest was unstable

--- system/kernel/kernel.nix
+++ system/kernel/kernel.nix
@@ -4,5 +4,8 @@
 
 {
   # Set kernel to latest
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Set to default value, latest had stability issues
+  boot.kernelPackages = pkgs.linuxPackages;
 }
