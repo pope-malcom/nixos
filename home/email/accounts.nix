412ca408c3c6330452f9b315c10e5aa520ea0a32
pope-malcom <pope.malcom@proton.me>
2026-04-29 10:14:28 +0100

    Configured XOAuth2 for SMTP and IMAP

--- home/email/accounts.nix
+++ home/email/accounts.nix
@@ -1,5 +1,6 @@
 # Add email accounts for programs to use
-{ ... }:
+{ pkgs, lib, ... }:
+
 {
   accounts.email.accounts = {
     pomal = {
@@ -8,21 +9,41 @@
       address = "pope.malcom@proton.me";
       realName = "pomal";
     };
-    sam-uio = {
-      enable = true;
-      address = "sam.holdcroft@its.uio.no";
-      aliases = [ "samho@uio.no" ];
-      flavor = "outlook.office365.com";
-      realName = "Sam";
-      thunderbird.enable = true;
-    };
+    sam-uio = 
+      {
+        # TODO This needs to be redone when home-manager thunderbird package updates
+        enable = true;
+        aliases = [ "samho@uio.no" ];
+        address = "sam.holdcroft@its.uio.no";
+        userName = "samho@uio.no";
+        flavor = "gmail.com"; # Not true, but it forces thunderbird.nix to set the smtp to xoauth2
+        realName = "Sam";
+        thunderbird = {
+          enable = true;
+          settings = id: {
+            # This should work, but it doesn't
+            "mail.smtpserver.smpt_${id}.authMethod" = 10;
+            "mail.server.server_${id}.authMethod" = lib.mkDefault 10; # 10 is XOAuth
+          };
+        };
+        imap = {
+          authentication = "xoauth2";
+          host = lib.mkForce "outlook.office365.com";
+          port = 993;
+        };
+        smtp = {
+          # authentication = "xoauth2";
+          host = lib.mkForce "smtp.office365.com";
+          port = 587;
+          tls.useStartTls = true;
+        };
+      };
   };
 
   programs.thunderbird = {
     enable = true;
     profiles.pomal = {
       isDefault = true;
-      #name = "Pope Malcom";
     };
   };
 }
