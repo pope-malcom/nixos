1a073436ac649c874a9e35b04bf1e75c71e048d4
pope-malcom <pope.malcom@proton.me>
2026-05-02 07:29:06 +0100

    Removed thunderbird from config

--- home/email/accounts.nix
+++ home/email/accounts.nix
@@ -21,25 +21,12 @@ in
         userName = "samho@uio.no";
       in    
       {
-        # TODO This needs to be redone when home-manager thunderbird package updates
         inherit address userName;
         enable = true;
         aliases = [ userName ];
-        flavor = "gmail.com"; # Not true, but it forces thunderbird.nix to set the smtp to xoauth2
         realName = "Sam";
         maildir.path = "Work";
         passwordCommand = "${lib.getExe oauth} ${userName}";
-        thunderbird = {
-          enable = true;
-          settings = id: {
-            # This should work, but it doesn't
-            "mail.smtpserver.smpt_${id}.authMethod" = 10;
-            "mail.server.server_${id}.authMethod" = lib.mkDefault 10; # 10 is XOAuth
-
-            "mail.imap.auto_unsubscribe_from_noselect_folders" = false;
-            #"mail.server.server_${id}.using_subscription" = false;
-          };
-        };
         neomutt = {
           enable = true;
           mailboxType = "maildir";
@@ -89,12 +76,5 @@ in
   programs.mbsync = {
     enable = true;
     package = pkgs.isync.override { withCyrusSaslXoauth2 = true; };
-  };
-
-  programs.thunderbird = {
-    enable = true;
-    profiles.pomal = {
-      isDefault = true;
-    };
   };
 }
